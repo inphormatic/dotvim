fedc83f760584faad0d6d1d11ad0026f3392fc11
inphormatic <dvdabati@gmail.com>
2023-12-14 23:26:25 +0100

    Add Telescope

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -12,3 +12,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+
--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,21 +1,2 @@
-vim.keymap.set('n', '<A-h>', '<C-w>h')
-vim.keymap.set('n', '<A-j>', '<C-w>j')
-vim.keymap.set('n', '<A-k>', '<C-w>k')
-vim.keymap.set('n', '<A-l>', '<C-w>l')
-vim.keymap.set('n', '<A-q>', '<C-w>q')
 
-vim.api.nvim_create_autocmd('filetype', {
-  callback = function()
-    local opts = { buffer = true, remap = true }
-
-    vim.keymap.set('n', 'h', '-', opts)
-    vim.keymap.set('n', 'l', '<CR>', opts)
-
-    vim.keymap.set('n', 'f', '%', opts)
-    vim.keymap.set('n', 'r', 'R', opts)
-    vim.keymap.set('n', 'x', 'D', opts)
-  end,
-  desc = 'Remapping for Netrw',
-  pattern = 'netrw',
-})
 
