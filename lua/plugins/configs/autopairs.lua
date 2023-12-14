521ffd5d0a3cfe8d54cd7680acc0d6ba67414226
inphormatic <dvdabati@gmail.com>
2023-12-14 23:26:09 +0100

    Add Telescope

--- lua/plugins/configs/autopairs.lua
+++
@@ -1,6 +0,0 @@
-require('nvim-autopairs').setup {
-  disable_filetype = { 'TelescopePrompt' , 'vim' },
-  enable_check_bracket_line = false,
-  ignored_next_char = '[%w%.]',
-}
-
--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,17 +1,10 @@
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ci', builtin.lsp_incoming_calls)
-vim.keymap.set('n', '<leader>co', builtin.lsp_outgoing_calls)
-vim.keymap.set('n', '<leader>fb', builtin.buffers)
-vim.keymap.set('n', '<leader>ff', builtin.find_files)
-vim.keymap.set('n', '<leader>fg', builtin.live_grep)
-vim.keymap.set('n', '<leader>fh', builtin.help_tags)
-
-
-require('telescope').setup {
-  defaults = {
-    prompt_prefix = '   ',
-    selection_caret = '🠮  ',
-  }
-}
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fc', builtin.command_history, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 
