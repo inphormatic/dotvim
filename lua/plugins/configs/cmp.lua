b741fca7377cfee1308755cb5063eb5b5ad8468c
David <dvdabati@gmail.com>
2023-12-20 08:25:31 +0100

    Update cmp.lua

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -1,11 +1,8 @@
-local autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 require('luasnip.loaders.from_vscode').lazy_load()
 
-
-cmp.event:on('confirm_done', autopairs.on_confirm_done())
 
 cmp.setup {
   enabled = function()
