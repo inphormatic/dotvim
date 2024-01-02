776ae06623b02159999070e657ec7ed8121a8f14
David <dvdabati@gmail.com>
2024-01-02 16:43:18 +0100

    Add mappings for windows

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,1 +1,7 @@
 vim.keymap.set('n', '<Tab>', '<C-6>', {})
+
+vim.keymap.set('n', '<A-h>', '<C-w>h', {})
+vim.keymap.set('n', '<A-j>', '<C-w>j', {})
+vim.keymap.set('n', '<A-k>', '<C-w>k', {})
+vim.keymap.set('n', '<A-l>', '<C-w>l', {})
+vim.keymap.set('n', '<A-q>', '<C-w>q', {})
