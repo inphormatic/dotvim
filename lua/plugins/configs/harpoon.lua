b6837eacef89e2c86cfcdce8ba3fbdca3f134d64
David <dvdabati@gmail.com>
2024-01-02 16:43:51 +0100

    Update harpoon.lua mappings

--- lua/plugins/configs/harpoon.lua
+++ lua/plugins/configs/harpoon.lua
@@ -4,8 +4,8 @@ local ui = require('harpoon.ui')
 vim.keymap.set('n', '<leader>a', mark.add_file)
 vim.keymap.set('n', '<leader>h', ui.toggle_quick_menu)
 
-vim.keymap.set('n', '1', function() ui.nav_file(1) end)
-vim.keymap.set('n', '2', function() ui.nav_file(2) end)
-vim.keymap.set('n', '3', function() ui.nav_file(3) end)
-vim.keymap.set('n', '4', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<A-1>', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<A-2>', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<A-3>', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<A-4>', function() ui.nav_file(4) end)
 
